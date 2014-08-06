8f738c7c1cb531f691f84fcb6eb7a21709c88a71
aleph <aleph@riseup.net>
2014-08-06 20:02:43 +0200

    https://wiki.enredaos.net/index.php?title=CICICdev/august#Formulari_d.27alta

--- Welcome/urls.py
+++ Welcome/urls.py
@@ -5,4 +5,7 @@ from Welcome import views
 
 urlpatterns = patterns('',
 	url(r'^public_form/$', views.public_form, name='public_form'),
+	url(r'^public_view_person/$', views.public_view_person, name='public_view_person'),
+	url(r'^public_view_project/$', views.public_view_project, name='public_view_project'),
+	url(r'^public_view_company/$', views.public_view_company, name='public_view_company'),
 )
--- Welcome/views.py
+++ Welcome/views.py
@@ -1,27 +1,138 @@
 #encoding=utf-8
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render_to_response, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from Welcome.forms import public_form
+from django.template import RequestContext
 
 def public_form(request):
-	#return HttpResponse("You're going to sign up %s.")
-	#p = get_object_or_404(Poll, pk=poll_id)
+	# Get the context from the request.
+	context = RequestContext(request)
 
+	# A HTTP POST?
 	from Welcome.forms import public_form
+	if request.method == 'POST':
+		form = public_form(request.POST)
 
-	public_form.title="You're going to sign up %s."
-	public_form.id = 25
-	from Welcome.admin import MembershipAdmin
-	public_form.fieldsets =  MembershipAdmin.fieldsets
+		# Have we been provided with a valid form?
+		if form.is_valid():
+			selected_type = request.POST['type']
+			print selected_type
+			if selected_type == "1" :
+				to_url = 'Welcome:public_view_project'
+			elif selected_type == "2":
+				to_url = 'Welcome:public_view_person'
+			elif selected_type == "3":
+				to_url = 'Welcome:public_view_person'
+			else:
+				to_url = "Welcome:public_form"
+			print to_url
+			return HttpResponseRedirect( reverse(to_url) )
+		else:
+			# The supplied form contained errors - just print them to the terminal.
+			print form.errors
+	else:
+		# If the request was not a POST, display the form to enter details.
+		form = public_form()
+	# Bad form (or form details), no form supplied...
+	# Render the form with error messages (if any).
+	return render_to_response('public_form.html', {'form': form}, context)
 
-	return render(request, 'public_form.html', {
-				'form': public_form,
-				'error_message': "You didn’t something.",
-			})
-	return HttpResponseRedirect(reverse('Welcome:public_form', args=(p.id,)))
+def public_view_person(request):
+	print "dsdg"
+	# Get the context from the request.
+	context = RequestContext(request)
+	print "dentro"
+	# A HTTP POST?
+	from Welcome.forms import public_form_person
+	if request.method == 'POST':
+		form = public_form_person(request.POST)
 
+		# Have we been provided with a valid form?
+		if form.is_valid():
+			pass
+		else:
+			# The supplied form contained errors - just print them to the terminal.
+			print form.errors
+	else:
+		# If the request was not a POST, display the form to enter details.
+		form = public_form_person()
+	# Bad form (or form details), no form supplied...
+	# Render the form with error messages (if any).
+	return render_to_response('public_form.html', {'form': form}, context)
+
+def public_view_project(request):
+	# Get the context from the request.
+	context = RequestContext(request)
+
+	# A HTTP POST?
+	from Welcome.forms import public_form_project
+	if request.method == 'POST':
+		form = public_form_project(request.POST)
+
+		# Have we been provided with a valid form?
+		if form.is_valid():
+			pass
+		else:
+			# The supplied form contained errors - just print them to the terminal.
+			print form.errors
+	else:
+		# If the request was not a POST, display the form to enter details.
+		form = public_form_project()
+	# Bad form (or form details), no form supplied...
+	# Render the form with error messages (if any).
+	return render_to_response('public_form.html', {'form': form}, context)
+	
+from Welcome.forms import public_form_person
+def public_view_company(request):
+	# Get the context from the request.
+	context = RequestContext(request)
+
+	# A HTTP POST?
+	from Welcome.forms import public_form_company
+	if request.method == 'POST':
+		form = public_form_company(request.POST)
+
+		# Have we been provided with a valid form?
+		if form.is_valid():
+			pass
+		else:
+			# The supplied form contained errors - just print them to the terminal.
+			print form.errors
+	else:
+		# If the request was not a POST, display the form to enter details.
+		form = public_form_company()
+	# Bad form (or form details), no form supplied...
+	# Render the form with error messages (if any).
+	return render_to_response('public_form.html', {'form': form}, context)
+	
+	
+	
+	'''
+			if human.id:
+				user = user( form.name, form.email, "password" )
+
+			return HttpResponseRedirect('anonymous/Welcome/ic_membership/', args=(human.id,))
+
+			# Save the new category to the database.
+			form.save(commit=True)
+
+			# Now call the index() view.
+			# The user will be shown the homepage.
+			pass
+			
+			return index(request)
+			switch case human:
+				case person:
+					reverse = 'General:Public_PersonAdmin'
+				case company:
+					reverse = 'General:Public_CompanyAdmin'
+				case project:
+					reverse = 'General:Public_ProjectAdmin'
+			
+			'''
+	
 	'''
 	switch case human:
 	case person:
