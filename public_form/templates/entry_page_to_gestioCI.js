9e15362722d4891dc796ecf71c4b42af861015a7
aleph <aleph@riseup.net>
2014-09-03 03:19:33 +0200

    https://wiki.enredaos.net/index.php?title=CICICdev/september#public_form.Tabs

--- public_form/templates/entry_page_to_gestioCI.js
+++ public_form/templates/entry_page_to_gestioCI.js
@@ -2,11 +2,7 @@
 
 	$(document).ready(function () {
 		$( ".ui-tabs" ).tabs();
-		var hide = $( ".ui-tabs" ).tabs( "option", "hide" );
-		$(".ui-tabs").tabs(
-			"option", "hide", 
-			{ effect: "fold", duration:  500 }
-		);
+		
 	} )
 
 }) (django.jQuery);
