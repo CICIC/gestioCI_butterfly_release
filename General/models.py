6fe8a35a51a47662641f8a18670ad2f19c43e060
aleph <aleph@riseup.net>
2014-09-23 22:30:16 +0200

    minnor

--- General/models.py
+++ General/models.py
@@ -75,8 +75,6 @@ class Being(models.Model):	# Abstract
 	class Meta:
 		abstract = True
 
-	def name(self):
-		return self.name.encode("utf-8")
 	def __unicode__(self):
 		return self.name.encode("utf-8")
 
