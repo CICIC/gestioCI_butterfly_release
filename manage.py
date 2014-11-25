835a8a88934dd0c81e0ecb4798c60dddf9711225
aleph <ox@enredaos.net>
2014-11-25 16:21:50 +0100

    [GestioCIv8] - Meta configuration

--- manage.py
+++ manage.py
@@ -2,6 +2,15 @@
 import os
 import sys
 
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# NOTICE - 25/11/2014 - This only for alterantive configuration backup.
+# Patch only for PyMySql python conector. Don use it otherwise.
+# See further, on: 
+# ( https://wiki.enredaos.net/index.php?title=CICICdev/november#PyDev_.2F_Eclipse )
+# import pymysql
+# pymysql.install_as_MySQLdb()
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 #os.environ['DJANGO_SETTINGS_MODULE'] = 'Config.settings'
 
 if __name__ == "__main__":
